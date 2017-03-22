9941c35d70df039911f810e9b9fac461aacc9ef1
towa <tw@cock.li>
2017-03-22 21:36:20 +0100

    add daily caffeine use rm pricelist

--- cycler/matenode/caffeine.py
+++ cycler/matenode/caffeine.py
@@ -1,12 +1,18 @@
-import datetime, mete
+import datetime, mete, socket
 
 def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     mate = mete.MateKasse('http://mate.fftr:3000/')
 
     startOfMonth = datetime.datetime.now().replace(day=1)
+    startOfDay = datetime.datetime.now().replace(hour=0,minute=0,second=0)
     tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
-    caff = mate.caffeineUsage(startOfMonth, tomorrow)
-    print caff
+
+    caffd = mate.caffeineUsage(startOfDay, tomorrow)
+    sock.sendto('Maschinenstatus/cycler/matenode/caffeine_day/set:%s' % caffd, ('127.0.0.1', 4444))
+
+    caffm = mate.caffeineUsage(startOfMonth, tomorrow)
+    sock.sendto('Maschinenstatus/cycler/matenode/caffeine_month/set:%s' % caffm, ('127.0.0.1', 4444))
 
 
 if __name__ == "__main__":
--- cycler/matenode/node.lua
+++ cycler/matenode/node.lua
@@ -1,53 +1,29 @@
 gl.setup(1920, 980)
 
-local font = resource.load_font("Inconsolata.otf")
 local font_futura = resource.load_font("futura-opentype.otf")
 local logo = resource.load_image("mate.png")
-local lines = {}
-local caffeine = 0
+local day = caff_day or 23
+local month = caff_month or 42
 
-function wrap(str, limit, indent, indent1)
-    indent = indent or ""
-    indent1 = indent1 or indent
-    limit = limit or 72
-    function wrap_parargraph(str)
-        local here = 1-#indent1
-        return indent1..str:gsub("(%s+)()(%S+)()", function(sp, st, word, fi)
-            if fi-here > limit then
-                here = st - #indent
-                return "\n"..indent..word
-            end
-        end)
-    end
-    local splitted = {}
-    for par in string.gmatch(str, "[^\n]+") do
-        local wrapped = wrap_parargraph(par)
-        for line in string.gmatch(wrapped, "[^\n]+") do
-            splitted[#splitted + 1] = line
-        end
-    end
-    return splitted
-end
-
-util.file_watch("prices", function(content)
-    lines = wrap(content, 60)
-end)
-
-util.file_watch("caffeine", function(caff)
-    caffeine = "Koffeinverbrauch (laufender Monat): " .. caff .. "mg"
-end)
+util.data_mapper{
+    ["caffeine_day/set"]    = function(caff_day)
+        day = caff_day
+    end;
+}
+util.data_mapper{
+    ["caffeine_month/set"]    = function(caff_month)
+        month = caff_month
+    end;
+}
 
 function node.render()
     gl.clear(0.1, 0.1, 0.1, 1)
-    font_futura:write(100,50,caffeine,50,1,1,1,1)
+    monthStr = "month: " .. month .. "mg"
+    dayStr = "day: " .. day .. "mg"
+    font_futura:write(100,100,"caffeine:",100,.5,.5,.5,1)
+    font_futura:write(100,300,monthStr,100,1,1,1,1)
+    font_futura:write(100,500,dayStr,100,1,1,1,1)
     local logo_size = 600
     local offset = 150
     logo:draw(WIDTH-logo_size-offset,offset,WIDTH - offset,logo_size + offset)
-    y = 200
-    x = 100
-    for i, line in ipairs(lines) do
-        local size = 40
-        font:write(x, y, line, size, 1, 1, 1, 1)
-        y = y + size
-    end
 end
